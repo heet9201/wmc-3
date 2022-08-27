008704a22262a1688ad1720bdd87e12765c7ee20
neilaybhatt27 <neilaybhatt27@gmail.com>
2022-08-28 00:16:44 +0530

    backend-auth mostly done

---
+++ src/routes/routes.js
@@ -0,0 +1,92 @@
+const { handleClientScriptLoad } = require('next/script');
+
+var router = require('express').Router();
+var User = require('../models/user');
+var bcrypt = require('bcrypt');
+var bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+router.post('/signin', urlencodedParser, async function (req, res) {
+    // var salt = bcrypt.genSaltSync(10);
+    // var hashedPwd = bcrypt.hash(req.body.password, salt);
+    var newUser = User({
+        firstName: req.body.fname,
+        lastName: req.body.lname,
+        email: req.body.email,
+        password: req.body.password
+    }).save(function (err, data) {
+        if(err) throw err;
+        console.log('Data added');
+    });
+    res.redirect('/login');
+
+    //res.send(await User.save());
+});
+
+router.post('/login', urlencodedParser, function (req, res) {
+    var regUser = User.findOne({email: req.body.email}, function (err, obj) {
+        if(err) throw err;
+        // console.log(obj);
+        // console.log(regUser.password);
+        // console.log({data: regUser});
+        if(!regUser){
+            return res.status(404).send('User not found');
+        }
+
+        if(req.body.password != obj.password){
+
+            return res.status(400).send('Invalid credentials');
+        }
+        console.log('Login successful');
+
+        var token = jwt.sign({_id: obj._id}, 'secret');
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            maxAge: 24*60*60*1000
+        });
+        console.log('success');
+        res.redirect('/');
+    });
+    // console.log(obj);
+    // // console.log(regUser.password);
+    // // console.log({data: regUser});
+    // if(!regUser){
+    //     return res.status(404).send('User not found');
+    // }
+
+    // if(req.body.password != obj.password){
+
+    //     return res.status(400).send('Invalid credentials');
+    // }
+    // console.log('Login successful');
+    // res.redirect('/');
+});
+
+router.get('/user', function (req, res) {
+    try{
+    var cookie = req.cookies['jwt'];
+    var claims = jwt.verify(cookie, 'secret');
+    if(!claims){
+        return res.status(401).send('Unauthenticated');
+    }
+
+    var user = User.findOne({_id: claims._id});
+    res.send(user);
+    } catch(e){
+        return res.status(401).send('Unauthenticated');
+    }
+});
+
+router.post('/logout', function (req, res) {
+   res.cookie('jwt', '', {
+    maxAge: 0
+   }); 
+   res.send({
+    message: 'success'
+   })
+   res.redirect('/login');
+});
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,17 @@ var next = require('next');
 var dev = process.env.NODE_ENV !== 'production';
 var app = next({dev});
 var handle = app.getRequestHandler();
+var mongoose = require('mongoose');
+var routes = require('./routes/routes');
+var cors = require('cors');
+var cookieParser = require('cookie-parser');
+
+//connecting to database...
+mongoose.connect('mongodb+srv://admin:admin@users.gaj3akz.mongodb.net/?retryWrites=true&w=majority', function (err) {
+    if(err) throw err;
+    console.log('Connected successfully');
+});
+
 
 app.prepare()
 .then(() => {
@@ -12,10 +23,18 @@ app.prepare()
         return handle(req, res);
     });
 
-    server.listen(3000, function (err) {
+    server.listen(8000, function (err) {
        if(err) throw err;
-       console.log('Now listening to port 3000'); 
+       console.log('Now listening to port 8000'); 
     });
+
+    server.use(routes);
+    server.use(express.json);
+    server.use(cookieParser());
+    server.use(cors({
+        credentials: true,
+        origin: ['http://localhost:3000']
+    }));
 })
 
 .catch((ex) => {
